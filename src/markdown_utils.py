9e49f4ce139815b44ba03779df41aee5cf01eae0
sander-dallorto <sanderrodriguesr75@gmail.com>
2025-02-18 20:29:36 -0300

    add block type check

--- src/markdown_utils.py
+++ src/markdown_utils.py
@@ -1,4 +1,5 @@
 import re
+from block_types import BlockType
 
 def extract_markdown_images(text):
     matches_text = re.findall(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
@@ -19,10 +20,40 @@ def markdown_to_blocks(markdown):
 
     return new_block_list
 
-def block_to_block_type(markdown):
-    pass
-            
+def block_to_block_type(markdown):    
+    # HEADING CHECK
+    hash_count = 0
+    for char in markdown:
+        
+        if char == "#":
+            hash_count += 1
+        else:
+            break
+
+    if 1 <= hash_count <= 6 and len(markdown) > hash_count and markdown[hash_count] == ' ':
+        return BlockType.HEADING
+
+    # CODE CHECK
+    if markdown.startswith("```") and markdown.endswith("```"):
+        return BlockType.CODE
+
+    # QUOTE CHECK
+
+    markdown_lines = markdown.split("\n")
+    if all(line.startswith(">") for line in markdown_lines):
+        return BlockType.QUOTE
     
+    # UNORDERED LIST CHECK
 
-   
+    if all(line.startswith("* ") or line.startswith("- ") for line in markdown_lines):
+        return BlockType.UNORDERED_LIST
+    
+    # ORDERED LIST CHECK
 
+    if all(line.startswith(f"{i+1}. ") for i, line in enumerate(markdown_lines)):
+        return BlockType.ORDERED_LIST
+    
+    # PARAGRAPHS
+
+    else:
+        return BlockType.PARAGRAPH
--- src/test_markdown_utils.py
+++ src/test_markdown_utils.py
@@ -1,6 +1,7 @@
 import unittest
 
-from markdown_utils import extract_markdown_links, extract_markdown_images, markdown_to_blocks
+from markdown_utils import *
+from block_types import BlockType
 
 class TestMarkdownUtils(unittest.TestCase):
     def test_eq_link(self):
@@ -28,6 +29,34 @@ class TestMarkdownUtils(unittest.TestCase):
 
     def test_white_spaces_only(self):
         assert markdown_to_blocks("  \n\n  \n\n   ") == []
+
+    def test_block_to_block_types(self):
+        #Testing headers
+        assert block_to_block_type("# Heading 1") == BlockType.HEADING
+        assert block_to_block_type("### Heading 3") == BlockType.HEADING
+        assert block_to_block_type("###### Heading 6") == BlockType.HEADING
+        assert block_to_block_type("####### Too many '#'s'") == BlockType.PARAGRAPH
+
+        #Testing codes
+        assert block_to_block_type("``` this is some code ```") == BlockType.CODE
+        assert block_to_block_type("`` this is NOT some code ``") == BlockType.PARAGRAPH
+
+        #Testing quotes
+        assert block_to_block_type(">This is a single quote") == BlockType.QUOTE
+        assert block_to_block_type(">These are\n>Multiple quotes") == BlockType.QUOTE
+
+        #Testing unordered lists
+        assert block_to_block_type("* First item") == BlockType.UNORDERED_LIST
+        assert block_to_block_type("- First item\n- Second item") == BlockType.UNORDERED_LIST
+
+        # Test ordered lists
+        assert block_to_block_type("1. First item\n2. Second item") == BlockType.ORDERED_LIST
+    
+        # Test paragraphs
+        assert block_to_block_type("Just a normal paragraph") == BlockType.PARAGRAPH
+
+
+
 
 if __name__ == "__main__":
     unittest.main()
